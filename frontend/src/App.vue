4a0840e45c61cf6569c216c101ee5dc53c3becc6
ronaldobergara <bergaraheadbanger@gmail.com>
2022-05-02 22:29:39 -0300

    Acessando Vuex

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ul>
+    <li v-for="todo in $allTodos" :key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  computed: {
+    $allTodos() {
+      return this.$store.getters.$allTodos
+    }
+  },
+  created() {
+    this.$store.dispatch("fetchTodos")
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
